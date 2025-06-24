81da83ef78e967d898dfcfdc0930f982698ebfef
samueldanso <me.samueldanso@gmail.com>
2025-06-24 09:32:36 +0000

    refactor: convert TrackPage

--- src/app/(app)/track/[id]/page.tsx
+++ src/app/(app)/track/[id]/page.tsx
@@ -2,10 +2,10 @@ import { notFound } from "next/navigation";
 import TrackDetails from "./_components/track-details";
 
 interface TrackPageProps {
-	params: { id: string };
+  params: { id: string };
 }
 
-export default function TrackPage({ params }: TrackPageProps) {
-	if (!params.id) return notFound();
-	return <TrackDetails id={params.id} />;
+export default async function TrackPage({ params }: TrackPageProps) {
+  if (!params.id) return notFound();
+  return <TrackDetails id={params.id} />;
 }
