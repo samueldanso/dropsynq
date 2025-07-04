547d413d5f8160e33feed6ad32f2b960364417f1
samueldanso <me.samueldanso@gmail.com>
2025-07-04 02:16:46 +0000

    refactor: update environment variable handling and improve provider configuration for Zora integration

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -3,43 +3,41 @@
 import { PrivyProvider } from "@privy-io/react-auth";
 import { createConfig, WagmiProvider } from "@privy-io/wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { base, baseSepolia, zora, zoraSepolia } from "viem/chains";
+import { base, baseSepolia } from "viem/chains";
 import { http } from "wagmi";
 import { env } from "@/env";
 
 const config = createConfig({
-	chains: [base, baseSepolia, zora, zoraSepolia],
-	transports: {
-		[base.id]: http(),
-		[baseSepolia.id]: http(),
-		[zora.id]: http(),
-		[zoraSepolia.id]: http(),
-	},
+  chains: [base, baseSepolia],
+  transports: {
+    [base.id]: http(),
+    [baseSepolia.id]: http(),
+  },
 });
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-	return (
-		<PrivyProvider
-			appId={env.NEXT_PUBLIC_PRIVY_APP_ID}
-			config={{
-				appearance: {
-					theme: "dark",
-					logo: "https://dropsynq.xyz/logo.png",
-				},
-				embeddedWallets: {
-					ethereum: {
-						createOnLogin: "users-without-wallets",
-					},
-				},
-				defaultChain: baseSepolia,
-				supportedChains: [base, baseSepolia, zora, zoraSepolia],
-			}}
-		>
-			<QueryClientProvider client={queryClient}>
-				<WagmiProvider config={config}>{children}</WagmiProvider>
-			</QueryClientProvider>
-		</PrivyProvider>
-	);
+  return (
+    <PrivyProvider
+      appId={env.NEXT_PUBLIC_PRIVY_APP_ID}
+      config={{
+        appearance: {
+          theme: "dark",
+          logo: "https://dropsynq.xyz/logo.png",
+        },
+        embeddedWallets: {
+          ethereum: {
+            createOnLogin: "users-without-wallets",
+          },
+        },
+        defaultChain: baseSepolia,
+        supportedChains: [base, baseSepolia],
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <WagmiProvider config={config}>{children}</WagmiProvider>
+      </QueryClientProvider>
+    </PrivyProvider>
+  );
 }
