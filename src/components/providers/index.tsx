15fbea3b8dd4c9c4ede35d214fe337545cc43dc4
samueldanso <me.samueldanso@gmail.com>
2025-07-08 11:41:37 +0000

    feat: switch to tesnet

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -4,40 +4,40 @@ import "@/lib/zora";
 import { PrivyProvider } from "@privy-io/react-auth";
 import { createConfig, WagmiProvider } from "@privy-io/wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { base } from "viem/chains";
+import { baseSepolia } from "viem/chains";
 import { http } from "wagmi";
 import { env } from "@/env";
 
 const config = createConfig({
-	chains: [base],
-	transports: {
-		[base.id]: http(),
-	},
+  chains: [baseSepolia],
+  transports: {
+    [baseSepolia.id]: http(),
+  },
 });
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-	return (
-		<PrivyProvider
-			appId={env.NEXT_PUBLIC_PRIVY_APP_ID}
-			config={{
-				appearance: {
-					theme: "dark",
-					logo: "https://tiudovdsloasaofqsvdb.supabase.co/storage/v1/object/public/logo//dropsynq-logo.png",
-				},
-				embeddedWallets: {
-					ethereum: {
-						createOnLogin: "users-without-wallets",
-					},
-				},
-				defaultChain: base,
-				supportedChains: [base],
-			}}
-		>
-			<QueryClientProvider client={queryClient}>
-				<WagmiProvider config={config}>{children}</WagmiProvider>
-			</QueryClientProvider>
-		</PrivyProvider>
-	);
+  return (
+    <PrivyProvider
+      appId={env.NEXT_PUBLIC_PRIVY_APP_ID}
+      config={{
+        appearance: {
+          theme: "dark",
+          logo: "https://tiudovdsloasaofqsvdb.supabase.co/storage/v1/object/public/logo//dropsynq-logo.png",
+        },
+        embeddedWallets: {
+          ethereum: {
+            createOnLogin: "users-without-wallets",
+          },
+        },
+        defaultChain: baseSepolia,
+        supportedChains: [baseSepolia],
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <WagmiProvider config={config}>{children}</WagmiProvider>
+      </QueryClientProvider>
+    </PrivyProvider>
+  );
 }
